73db367b5b25d03d0da44be44f95d46f1588fa66
Mac <maclong9@icloud.com>
2023-09-07 23:50:46 +0100

    chore(+server): removed console logs.

--- src/routes/+server.js
+++ src/routes/+server.js
@@ -4,7 +4,6 @@ import {json} from '@sveltejs/kit';
 export async function GET() {
   const client = redisClient();
   const newClicks = await client.incr('clicks');
-  console.log(newClicks);
 
   return json(newClicks)
 }
