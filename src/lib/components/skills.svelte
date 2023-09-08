d9e38e1e778f5fce2f1d06bdb1ee719e40174917
Mac <mac@3sidedcube.com>
2023-09-08 14:42:23 +0100

    fix(Skills): punctuation corrected.

--- src/lib/components/skills.svelte
+++ src/lib/components/skills.svelte
@@ -1,22 +1,28 @@
 <script>
+import Title from '$lib/components/title.svelte';
+
 const skills = [
   "TypeScript",
   "TailwindCSS",
   "Next.js",
   "SvelteKit",
+  "Git",
   "HTML",
   "CSS",
   "JavaScript",
   "Lua",
-  "Vim"
+  "Shell",
+  "Vim",
 ];
 </script>
 
-<div class='space-y-2'>
-  <title text='Skills' />
+<div class='space-y-2 max-w-lg'>
+  <Title text='Skills' />
+  <div class="ml-2">
   {#each skills as skill, i}
     <span>
-      {skill}{i - skills.length !== 1 ? "," : " "} 
+     {skill}{#if skills.length - i === 1}.{/if}{#if skills.length - i === 2}{` & `}{/if}{#if skills.length - i !== 1 && skills.length - i !== 2}, {/if}
     </span>
   {/each}
+  </div>
 </div>
