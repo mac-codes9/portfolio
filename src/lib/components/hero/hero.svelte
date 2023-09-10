11c4ce531bacc367d264d3b83914435687f950f8
Mac <115668288+mac-codes9@users.noreply.github.com>
2023-09-10 02:16:05 +0100

    Update hero.svelte

--- src/lib/components/hero/hero.svelte
+++ src/lib/components/hero/hero.svelte
@@ -8,9 +8,10 @@
  <p>
     I am a seasoned web developer with over 3 years of experience. Currently
     honing my skills in NextJS, Typescript, and other cutting-edge technologies.
+    I am also learning developer tools and systems programming with Rust and Zig.
   </p>
   <p>
-    In addition to my passion for coding, I also enjoy exploring the world of
+    In addition to my passion for programming, I also enjoy exploring the world of
     music composition and challenging myself with skateboard tricks whenever
     I'm not recovering from an injury.
   </p>
