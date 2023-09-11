4784e4d79251ab23aa733e6c8320941073313146
Mac <115668288+mac-codes9@users.noreply.github.com>
2023-09-11 18:32:14 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
   import '../app.css'
+  import { fade } from 'svelte/transition';
   import Header from '$lib/components/layout/header.svelte';
   import Footer from '$lib/components/layout/footer.svelte';
 </script>
 
-<body class='space-y-8 py-10 px-4 mx-auto max-w-3xl bg-neutral-950 text-neutral-400 text-sm mdl:py-16 mdl:px-8 mdl:space-y-20 mdl:max-w-4xl'>
+<body transition:fade class='space-y-8 py-10 px-4 mx-auto max-w-3xl bg-neutral-950 text-neutral-400 text-sm mdl:py-16 mdl:px-8 mdl:space-y-20 mdl:max-w-4xl'>
   <Header />
   <main class='space-y-8'>
     <slot />
