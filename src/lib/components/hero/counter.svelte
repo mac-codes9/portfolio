ace1b34fa348a843c5c3cdfbc454f571527d1d37
Mac <115668288+mac-codes9@users.noreply.github.com>
2023-09-12 08:51:39 +0000

    fix(Counter): tick up is now reactive

--- src/lib/components/hero/counter.svelte
+++ src/lib/components/hero/counter.svelte
@@ -1,48 +1,48 @@
 <script>
-  import { onMount } from 'svelte';
-  import { invalidate } from '$app/navigation';
+  import { onMount } from "svelte";
   export let clicks, clicked;
   let error, oldClicks, newClicks;
 
-	onMount(() => {
-		const interval = setInterval(async () => {
+  onMount(() => {
+    const interval = setInterval(async () => {
       oldClicks = clicks;
-      newClicks = await fetch('/').then(res => res.json());
-		}, 5000);
+      newClicks = await fetch("/").then((res) => res.json());
+    }, 5000);
 
-		return () => {
-			clearInterval(interval);
-		};
-	});
+    return () => {
+      clearInterval(interval);
+    };
+  });
 
-  setInterval(() => {
+  $: setInterval(() => {
     if (oldClicks < newClicks) {
       oldClicks++;
       clicks = oldClicks;
     }
-  }, 5);
+  }, 100);
 
   const incrementCount = async () => {
     clicks++;
     clicked = true;
     try {
-      const newClicks = await fetch('/', {method: 'PUT'});
+      const newClicks = await fetch("/", { method: "PUT" });
       clicks = await newClicks.json();
     } catch (e) {
       clicks--;
       clicked = false;
       error = e;
     }
-  }
+  };
 </script>
 
+{clicks} - {oldClicks} - {newClicks}
 <div class="mt-5 space-x-2">
   <span>{!error ? `${Number(clicks)} Clicks` : error}</span>
-  <button 
+  <button
     class="text-neutral-900 bg-neutral-100 font-bold px-2 py-1 rounded-md transition-colors duration-300 hover:bg-neutral-400 disabled:bg-neutral-400"
     on:click={incrementCount}
     disabled={clicked}
   >
-  {!clicked ? 'Say Hi!' : 'Thank you!'}
+    {!clicked ? "Say Hi!" : "Thank you!"}
   </button>
 </div>
