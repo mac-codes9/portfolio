814527bffb226bba94bc63b78b375e1a552d1b7b
Mac <115668288+mac-codes9@users.noreply.github.com>
2023-09-19 12:49:32 +0100

    chore(Footer): updated footer text

--- src/lib/components/layout/footer.svelte
+++ src/lib/components/layout/footer.svelte
@@ -6,7 +6,7 @@
 <footer class='space-y-4'>
   <Title text='Contact' />
   <span>
-    Email me or follow me on the various social media using the links below.
+    Email me or follow me on social media using the links below.
   </span>
   <Links />
 </footer>
