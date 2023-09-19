5ef5823385bab783aacbbb5ffa461fabfe3b26be
Mac <115668288+mac-codes9@users.noreply.github.com>
2023-09-19 17:02:22 +0100

    Update glow.svelte

--- src/lib/components/glow.svelte
+++ src/lib/components/glow.svelte
@@ -1,1 +1,1 @@
-<div class="absolute top-0 -left-[58px] w-16 h-16 rounded-full shadow-2xl shadow-primary" />
+<div class="absolute top-0 -left-16 w-16 h-16 rounded-full shadow-2xl shadow-primary" />
