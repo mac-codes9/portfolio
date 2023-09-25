8c59a45106ca2862444b2d88867bf42ad0d61d39
Mac <115668288+mac-codes9@users.noreply.github.com>
2023-09-25 16:37:06 +0000

    style(skills): new layout + correct body text

--- src/lib/components/skills.svelte
+++ src/lib/components/skills.svelte
@@ -1,31 +1,30 @@
 <script>
   import Title from "$lib/components/title.svelte";
-  import punctuate from "$lib/utilities/punctuate";
 
   const skills = [
-    "Shell",
-    "Git",
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "TypeScript",
-    "Lua",
-    "Database Management",
-    "Cloud Services",
-    "Containerisation",
-    "DevOps",
-    "CI/CD",
-    "Testing",
-    "Problem Solving",
-    "Communication",
-    "Time Management",
+    {
+      heading: "Web Development",
+      description:
+        "With a good knowledge of Web technologies such as HTML, CSS & JavaScript I craft beautiful, high speed websites with cutting-edge technologies including Next.js, TypeScript & TailwindCSS.",
+    },
+    {
+      heading: "Systems Knowledge",
+      description:
+        "I have a solid understanding of unix and continuos integration systems including the shell, git, containers, scripting languages like lua and python, and SQL and NoSQL Database Services. I also have experience workng with cloud providers including Google, AWS, Vercel & more.",
+    },
+    {
+      heading: "Soft Skills",
+      description:
+        "Communication, time management and attention to detail are some great strengths of mine due to customer service experience I am great working within a team. I'm also quite good at thinking on my feet and problem solving.",
+    },
   ];
 </script>
 
 <div class="space-y-2 max-w-lg">
   <Title text="Skills" />
 
-  {#each skills as skill, i}
-    {skill}<span>{punctuate(skills.length, i)}</span>
+  {#each skills as { heading, description }}
+    <h2>{heading}</h2>
+    <p>{description}</p>
   {/each}
 </div>
