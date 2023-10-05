de580adff92102582c9bba4e79291d34cecab853
Mac <115668288+mac-codes9@users.noreply.github.com>
2023-10-05 13:07:12 +0000

    style(Skills): updated spacings for items

--- src/lib/components/skills.svelte
+++ src/lib/components/skills.svelte
@@ -17,23 +17,26 @@
       description:
         "Communication, time management and attention to detail are some strengths I find myself using regularly.",
     },
+    {
+    heading: "Developer Tools",
+    description: "I am proficient in SQL and NoSQL databases. Additionally, I implement continuous integration and vcs to streamline development."
+  },
   ];
 </script>
 
 <Title text="Skills" />
-<div class='space-y-8 max-w-lg mx-auto'>
+<div class='flex flex-col items-center space-y-4 mdl:flex-row mdl:flex-wrap mdl:items-start mdl:space-y-0 mx-auto pb-10 mdl:px-8'>
   {#each skills as { heading, description }}
+    <div class="mdl:w-[346px] mdl:mb-8 mdl:mx-4">
     <h2>{heading}</h2>
     <p>{description}</p>
+</div>
   {/each}
 </div>
 
 <style lang="postcss">
-  h2 {
-    @apply text-3xl m-0 font-bold bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent;
+   div h2 {
+    @apply text-3xl font-bold bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent mb-2;
     @apply first:from-red-500 first:to-yellow-500;
-  }
-  h2:last-child {
-    @apply from-blue-500 to-emerald-500;
   }
 </style>
