8f589904557594812dad794d03c6e4860fa813e9
Mac <maclong9@icloud.com>
2023-12-04 20:02:45 +0000

    Update header.svelte

--- src/lib/components/layout/header.svelte
+++ src/lib/components/layout/header.svelte
@@ -6,20 +6,6 @@
 <header class="mb-16">
   <Glow />
   <div class="w-full flex justify-between items-start">
-    <Links
-      links={[
-        { href: "/art", label: "Art" },
-        { href: "/music", label: "Music" },
-        { href: "/skating", label: "Skating" },
-      ]}
-    />
-    <!--
-    <label for="locale" class="sr-only">Locale</label>
-    <select id="locale" class="bg-transparent" bind:value={$locale}>
-      {#each locales as locale}
-        <option class="bg-transparent" value={locale}>{locale}</option>
-      {/each}
-    </select>
-    -->
+    <Links />
   </div>
 </header>
