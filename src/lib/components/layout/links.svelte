090ba9b298c3e2d371ec95f36b7a2c2f8d00ac69
Mac <maclong9@icloud.com>
2023-12-04 20:06:02 +0000

    Update links.svelte

--- src/lib/components/layout/links.svelte
+++ src/lib/components/layout/links.svelte
@@ -1,5 +1,5 @@
 <div class="space-y-3">
-  <a href="/">mac-codes</a> / <a href="https://github.com/mac-codes9/portfolio">portfolio</a>
+  <a href="/">mac-codes</a> {" "} / {" "} <a href="https://github.com/mac-codes9/portfolio">portfolio</a>
   <a class="no-underline p-2 pl-0" href="mailto:maclong9@icloud.com">
     maclong9@icloud.com
   </a>
