37723d927a769f4a2b76f2ad685bbd7f12470320
Mac <maclong9@icloud.com>
2023-12-04 19:54:29 +0000

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
   import "../app.css";
   import { fade } from "svelte/transition";
   import Header from "$lib/components/layout/header.svelte";
-  import Footer from "$lib/components/layout/footer.svelte";
 </script>
 
 <body transition:fade={{ delay: 250, duration: 300 }} class="space-y-8 py-10 px-4 mx-auto max-w-3xl bg-neutral-950 text-neutral-400 text-sm mdl:py-16 mdl:px-8 mdl:space-y-20 mdl:max-w-4xl">
