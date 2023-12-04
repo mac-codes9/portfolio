44ea0235efe4bbf6b0b7e884dece4b301182d2fb
Mac <maclong9@icloud.com>
2023-12-04 20:11:19 +0000

    fix(links): / corrected

--- src/lib/components/layout/links.svelte
+++ src/lib/components/layout/links.svelte
@@ -1,6 +1,8 @@
 <div class="space-y-3 flex-col">
   <div class="flex items-center">
-    <a href="/">mac-codes</a> {" "} / {" "} <a class="font-semibold text-white" href="https://github.com/mac-codes9/portfolio">portfolio</a>
+    <a href="/">mac-codes</a> 
+    <span class="px-2">/</span>
+    <a class="font-semibold text-white" href="https://github.com/mac-codes9/portfolio">portfolio</a>
   </div>
   <a class="no-underline p-2 pl-0" href="mailto:maclong9@icloud.com">
     maclong9@icloud.com
