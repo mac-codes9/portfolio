ed618beb11443401bd4057d00eec81ff577cc5fa
Mac <maclong9@icloud.com>
2023-12-04 19:56:32 +0000

    style(layout): removed y padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
   import Header from "$lib/components/layout/header.svelte";
 </script>
 
-<body transition:fade={{ delay: 250, duration: 300 }} class="space-y-8 py-10 px-4 mx-auto max-w-3xl bg-neutral-950 text-neutral-400 text-sm mdl:py-16 mdl:px-8 mdl:space-y-20 mdl:max-w-4xl">
+<body class="space-y-8 px-4 mx-auto max-w-3xl bg-neutral-950 text-neutral-400 text-sm mdl:px-8 mdl:space-y-20 mdl:max-w-4xl">
   <Header />
   <main class="space-y-12 mx-auto">
     <slot />
