b70df00034a2006ff2fb1ebdad5cc62a2464a015
Mac <maclong9@icloud.com>
2023-12-04 20:08:59 +0000

    chore: prettier

--- src/lib/components/layout/links.svelte
+++ src/lib/components/layout/links.svelte
@@ -1,5 +1,6 @@
-<div class="space-y-3">
-  <a href="/">mac-codes</a> {" "} / {" "} <a class="font-semibold" href="https://github.com/mac-codes9/portfolio">portfolio</a>
+<div class="space-y-3 flex-col">
+  <div class="flex items-center">
+<a href="/">mac-codes</a> {" "} / {" "} <a class="font-semibold text-white" href="https://github.com/mac-codes9/portfolio">portfolio</a></div>
   <a class="no-underline p-2 pl-0" href="mailto:maclong9@icloud.com">
     maclong9@icloud.com
   </a>
