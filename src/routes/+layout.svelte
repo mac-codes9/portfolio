d75644b16bd5cf178a63d2897edc0107442e5c2b
Mac <maclong9@icloud.com>
2023-12-04 20:13:49 +0000

    Update +layout.svelte rm all styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
   import Header from "$lib/components/layout/header.svelte";
 </script>
 
-<body class="space-y-8 pt-8 px-4 mx-auto max-w-3xl bg-neutral-950 text-neutral-400 text-sm mdl:px-8 mdl:space-y-20 mdl:max-w-4xl">
+<body class="">
   <Header />
   <main class="space-y-12 mx-auto">
     <slot />
