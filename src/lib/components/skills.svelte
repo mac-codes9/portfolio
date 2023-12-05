3f91e8a34b49ce6f510baf466dcc716f7877b239
Mac <maclong9@icloud.com>
2023-12-05 14:27:04 +0000

    Update skills.svelte

--- src/lib/components/skills.svelte
+++ src/lib/components/skills.svelte
@@ -27,7 +27,7 @@
 
 <Title text="Skills" />
 <div
-  class="flex flex-col items-center mx-auto mdl:flex-row mdl:flex-wrap mdl:items-start mdl:space-y-0"
+  class="flex flex-col items-center mx-auto mdl:flex-row mdl:flex-wrap mdl:items-start mdl:space-y-4"
 >
   {#each skills as { heading, description }}
     <div class="skill mdl:w-[326px] mdl:mb-8 py-4 md:py-0">
