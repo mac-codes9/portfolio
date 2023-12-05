81a4507f21bce5c7c43e6b744c062e97cbd2f243
Mac <maclong9@icloud.com>
2023-12-05 15:54:43 +0000

    Update title.svelte

--- src/lib/components/title.svelte
+++ src/lib/components/title.svelte
@@ -2,6 +2,6 @@
   export let text;
 </script>
 
-<h2 class="text-lg font-bold border-b-2 border-b-teal-700 max-w-[30px]">
+<h2 class="text-lg font-bold border-b-2 border-b-teal-700 max-w-[30px] pb-2 md:pb-0">
   {text}
 </h2>
