f48ede9b6d889eae1df228f34dbf73bde9ac3509
Mac <maclong9@icloud.com>
2023-12-05 14:34:34 +0000

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
   import Header from "$lib/components/layout/header.svelte";
 </script>
 
-<body transition:fade class="space-y-6 pt-4 px-4 mx-auto max-w-3xl bg-neutral-950 text-neutral-400 text-sm md:px-8 md:max-w-4xl">
+<body transition:fade class="space-y-6 pt-4 px-4 mx-auto max-w-3xl bg-neutral-950 text-neutral-400 text-sm md:px-8 md:max-w-4xl mdl:space-y-0">
   <Header />
   <main class="space-y-12 mx-auto">
     <slot />
