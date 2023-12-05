79a5df4f2d0a8dec04e8cf219d15c679a5f28d45
Mac <maclong9@icloud.com>
2023-12-05 14:28:43 +0000

    Update item.svelte

--- src/lib/components/projects/item.svelte
+++ src/lib/components/projects/item.svelte
@@ -6,7 +6,7 @@
   href={url}
   target="_blank"
   rel="noreferrer"
-  class="w-full no-underline border-neutral-400 rounded-md border-[.5px] py-4 px-2 group transition-all relative duration-700 hover:border-neutral-100 hover:scale-105 hover:-translate-y-1 hover:text-neutral-400 hover:border-[1px] mdl:w-[364px] mdl:my-4"
+  class="w-full no-underline border-neutral-400 rounded-md border-[.5px] py-4 px-2 group transition-all relative duration-700 hover:border-neutral-100 hover:scale-105 hover:-translate-y-1 hover:text-neutral-400 hover:border-[1px] mdl:w-[364px] mdl:mx-2 mdl:my-4"
 >
   <h3 class="text-neutral-100 mb-4">{name}</h3>
   <p class="max-w-md">{description}</p>
