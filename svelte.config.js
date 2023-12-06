210e9d8bd7ad4440dd41b18d8e8c5ae9d7eb92ef
Mac <maclong9@icloud.com>
2023-12-06 14:50:06 +0000

    fix: updated to static adapter, testing gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : ''
+    }
   },
 
   preprocess: [vitePreprocess({})],
