3c1152b702fcf0a218c1053c414e4009b6b0e833
Mac <maclong9@icloud.com>
2023-12-06 15:00:37 +0000

    fix: gh pages back to vercel due to env variables and serverside required for redis

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : ''
-    }
   },
 
   preprocess: [vitePreprocess({})],
